97f213bb6ecfbf646bd51109779bebdce787c8b9
Yuri Baranov <baranovu@gmail.com>
2014-05-31 16:51:56 +0400

    home page demo forcing landscape orientation

---
+++ app/views/sprites/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @sprite, :id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Fallacymania2::Application.routes.draw do
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+
+  resources :sprites, :only => [:index, :show]
 end
