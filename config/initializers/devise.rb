e97bef725e6c43c964e067629d9fa6cd5a5bcfb5
Yuri Baranov <baranovu@gmail.com>
2014-08-12 10:50:23 +0400

    move development secrets from devise.rb to secrets.yml

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -230,8 +230,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, '672334646389-mbeugm1paccdpgipdh9lorbgs18e64jq.apps.googleusercontent.com', 'g_qXP--2vcVxW54Lxm5HdpuS' #FIXME secret.yml
-
+  config.omniauth :google_oauth2, Rails.application.secrets.google_oauth2_client_id , Rails.application.secrets.google_oauth2_client_secret
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
