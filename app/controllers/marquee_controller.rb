a4c035c061bf4cde95c51f42c389ee526975a7fa
Yuri Baranov <baranovu@gmail.com>
2014-08-16 23:24:32 +0400

    filter forcing user auth for Marquee controller actions

--- app/controllers/marquee_controller.rb
+++ app/controllers/marquee_controller.rb
@@ -1,4 +1,5 @@
 class MarqueeController < ApplicationController
+  before_filter :authenticate_user!
   def show
     @games = Game.all
     @open_games = @games #TODO: find all open games no more than an hour old
