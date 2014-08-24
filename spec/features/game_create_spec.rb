7ece41130adac20364368ed3ee747d53a32d15b8
Yuri Baranov <baranovu@gmail.com>
2014-08-25 01:36:34 +0400

    finishing in red

--- spec/features/game_create_spec.rb
+++ spec/features/game_create_spec.rb
@@ -11,17 +11,41 @@ feature 'Game creation' do
     Warden.test_reset!
   end
 
-  # Scenario: User starts creating a new game
+  # Scenario: User initiates creation of a new game
   #   Given I am singed in
   #   When I visit marquee page
-  #   And press 'Create new game' button
+  #   And click 'Create new game' button
   #   I see new_game form to fill and submit
 
-  scenario 'User starts creating a new game' do
+  scenario 'User initiates creation of a new game' do
     user = FactoryGirl.create(:user)
     login_as(user)
     visit marquee_path
     click_on 'Create new game'
     expect(page).to have_content 'New Game'
+    expect(current_url).to eq( new_game_url)
   end
+
+  # Scenario: User creates a new game
+  #   Given I am singed in
+  #   When I visit marquee page
+  #   Then click 'Create new game' button
+  #   And then fill in game description
+  #   And then click 'Create Game' button
+  #   Finally, I will see edit_game page (as a master)
+  #   And game with me as game master will become available
+
+  scenario 'User initiates creation of a new game' do
+    user = FactoryGirl.create(:user)
+    login_as(user)
+    visit marquee_path
+    click_on 'Create new game'
+    fill_in 'Description', with: 'Test-game'
+    click_on 'Create Game'
+    game=Game.find_by_description('Test-game')
+    expect(game.game_master).to eq(user)
+    expect(current_url).to eq( edit_game_url(game)) #FIXME: this fails!
+  end
+
+
 end
